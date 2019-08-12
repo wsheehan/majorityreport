f05e46aee57b1e1ac1646268ea8cd7b1a72f8627
Will Sheehan <will@citizenhex.com>
2019-08-12 14:45:04 -0400

    init font-awesome in index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import init from './font-awesome-init'
+init()
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
