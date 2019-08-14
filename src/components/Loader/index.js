2254bab122a6809f3965fd4fa3c307bc472d301e
Will Sheehan <will@citizenhex.com>
2019-08-14 09:24:22 -0400

    Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.scss'
+
+function Loader () {
+  return (
+    <div className="loader-wrapper">
+      <div className="loader">
+        <div></div><div></div><div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
