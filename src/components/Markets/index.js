87fbbaa4b0ebb68710f818ecb8d815ad4168a305
Will Sheehan <will@citizenhex.com>
2019-08-16 15:03:57 -0400

    add sorting / filtering options for markets table

--- src/components/Markets/index.js
+++ src/components/Markets/index.js
@@ -3,6 +3,8 @@ import ResSource from '../ResSource'
 import Loader from '../Loader'
 import { Link } from 'react-router-dom'
 import BN from 'bignumber.js'
+import { isEqual } from 'lodash'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { weiToDec, parseMarket } from '../../helpers'
 import graphql from '../../graphql'
@@ -13,13 +15,31 @@ class Markets extends Component {
     super()
 
     this.state = {
-      markets: null
+      markets: null,
+      tableParams: {
+        skip: 0,
+        first: 10,
+        orderBy: 'totalDisputed',
+        finalized: false
+      },
+      showSettings: false
     }
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.fetchMarkets()
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (!isEqual(prevState.tableParams, this.state.tableParams)) {
+      this.fetchMarkets()
+    }
+  }
+
+  async fetchMarkets() {
+    const params = this.state.tableParams
     const query = `{
-      markets(orderBy: totalDisputed, orderDirection: desc, where: { finalized: null }, first: 10) {
+      markets(orderBy: ${params.orderBy}, orderDirection: desc, where: { finalized: ${params.finalized} }, first: ${params.first}, skip: ${params.skip}) {
         id
         description
         topic
@@ -79,48 +99,142 @@ class Markets extends Component {
     this.setState({ markets, feeWindow: feeWindows[1], featuredMarket: parseMarket(market) })
   }
 
+  toggleShowSettings = () => {
+    this.setState({ showSettings: !this.state.showSettings })
+  }
+
+  updateTableParam = (param, newVal) => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        tableParams: {
+          ...prevState.tableParams,
+          [param]: newVal
+        }
+      }
+    })
+  }
+
+  pagination = ({ first, skip }) => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        tableParams: {
+          ...prevState.tableParams, 
+          first, 
+          skip 
+        }
+      }
+    })
+  }
+
+  page = (n) => {
+    const { first, skip } = this.state.tableParams
+    const x = skip + (n * first)
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        tableParams: {
+          ...prevState.tableParams, 
+          first, 
+          skip: x >= 0 ? x : 0 
+        }
+      }
+    })
+    
+  }
+
   render() {
-    const { markets, feeWindow, featuredMarket } = this.state
+    const { markets, feeWindow, featuredMarket, showSettings, tableParams } = this.state
     if (!markets) {
       return <Loader />
     }
+    const settingsDisplay = showSettings ? 'block' : 'none'
 
     return (
-      <div className="markets container-fluid">
-        <div className="row">
-          <div className="featured-market col-md-10 offset-md-1">
-            <div id="featured">Featured Market</div>
-            <div id="featured-desc">{featuredMarket.description}</div>
-            <p>{featuredMarket.longDescription}</p>
-            <div id="feature-res-source"><span>Res Source: </span><ResSource text={featuredMarket.resSource || "N/A"}/></div>
-            <span id="featured-tentative">Tentative Outcome: <span className={`tentative tentative-${featuredMarket.tentativeOutcome}`}>{featuredMarket.tentativeOutcome}</span> </span>
-            <span id="featured-creator">Created By: {featuredMarket.marketCreator.id.slice(0, 8)} </span>
-            <span id="featured-staked">Disputed: {weiToDec(new BN(featuredMarket.totalDisputed)).toFixed(2)} REP</span>
+      <div>
+        <div className="markets container-fluid">
+          <div className="row">
+            <div className="featured-market col-md-10 offset-md-1">
+              <div id="featured">Featured Market</div>
+              <div id="featured-desc">{featuredMarket.description}</div>
+              <p>{featuredMarket.longDescription}</p>
+              <div id="feature-res-source"><span>Res Source: </span><ResSource text={featuredMarket.resSource || "N/A"}/></div>
+              <span id="featured-tentative">Tentative Outcome: <span className={`tentative tentative-${featuredMarket.tentativeOutcome}`}>{featuredMarket.tentativeOutcome}</span> </span>
+              <span id="featured-creator">Created By: {featuredMarket.marketCreator.id.slice(0, 8)} </span>
+              <span id="featured-staked">Disputed: {weiToDec(new BN(featuredMarket.totalDisputed)).toFixed(2)} REP</span>
+            </div>
           </div>
-        </div>
-
-        <div className="row">
-          <table className="mr-table">
-            <thead>
-              <tr>
-                <th className="description-cell">Description</th>
-                <th>Topic</th>
-                <th>Type</th>
-                <th>Creator</th>
-                <th>Disputed</th>
-                <th>Round</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              {markets.map(market => (
-                <MarketRow market={market} {...this.props} />
-              ))}
-            </tbody>
-          </table>
+          <div className="row">
+            <div className="settings-popup" style={{display: settingsDisplay}}>
+              <span>
+                <span>Sort By: </span> 
+                <select onChange={e => this.updateTableParam("orderBy", e.target.value)}>
+                  <option value="totalDisputed">Total Disputed</option>
+                  <option value="rounds">Number of Rounds</option> 
+                </select>
+              </span>
+              <span>
+                <span>Finalized: </span> 
+                <select value={tableParams.finalized} onChange={e => this.updateTableParam("finalized", Boolean(e.target.value))}>
+                  <option value="true">True</option>
+                  <option value="false">False</option> 
+                </select>
+              </span>
+              <span>
+                <span>Show: </span> 
+                <select value={tableParams.first} 
+                  onChange={e => this.pagination({ first: Number(e.target.value), skip: 0 })}>
+                  <option value="10">10</option>
+                  <option value="25">25</option> 
+                  <option value="50">50</option> 
+                </select>
+              </span>
+            </div>
+            <table className="mr-table">
+              <thead>
+                <tr>
+                  <th className="description-cell">Description</th>
+                  <th>Topic</th>
+                  <th className="text-center">Type</th>
+                  <th>Creator</th>
+                  <th>Disputed</th>
+                  <th>Rounds</th>
+                  <th>
+                    Status
+                    <span className={`settings settings-up-${showSettings}`} onClick={this.toggleShowSettings}>
+                      <FontAwesomeIcon icon={["fas", showSettings ? "times" : "cog"]} style={{color: "#333333"}} size="2x" />
+                    </span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {markets.map(market => (
+                  <MarketRow market={market} {...this.props} />
+                ))}
+              </tbody>
+            </table>
+            <div className="paginator">
+              <span>
+                <span className="page" onClick={() => this.page(-1)}>PREV</span>
+                <span className="page-num">{tableParams.skip / tableParams.first}</span>
+                <span className="page" onClick={() => this.page(1)}>NEXT</span>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     )
+  }
+}
+
+function MarketType ({ type }) {
+  if (type === "Binary") {
+    return <FontAwesomeIcon icon={["fas", "adjust"]} size="1x" />
+  } else if (type === "Categorical") {
+    return <FontAwesomeIcon icon={["fas", "ellipsis-h"]} size="1x" />
+  } else {
+    return <FontAwesomeIcon icon={["fas", "ruler-horizontal"]} size="1x" />
   }
 }
 
@@ -131,7 +245,7 @@ function MarketRow ({ market, web3, history }) {
       <tr onClick={() => history.push(`/market/${market.id}`)}>
         <td className="description-cell">{market.description}</td>
         <td><span className="market-topic">{topic}</span></td>
-        <td>{market.marketType}</td>
+        <td className="text-center"><MarketType type={market.marketType} /></td>
         <td><Link to={`/u/${market.marketCreator.id}`}>{market.marketCreator.id.slice(0, 8)}...</Link></td>
         <td>{weiToDec(new BN(market.totalDisputed)).toFixed(2)} REP</td>
         <td><span className="num-rounds">{market.disputes.length}</span></td>
--- src/font-awesome-init.js
+++ src/font-awesome-init.js
@@ -1,7 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCheck, faCog, faTimes, faAdjust, faEllipsisH, faRulerHorizontal } from '@fortawesome/free-solid-svg-icons'
 import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
 
 export default function init() {
-  return library.add(faCheckCircle)
+  return library.add(faCheckCircle, faCog, faTimes, faAdjust, faEllipsisH, faRulerHorizontal)
 }
