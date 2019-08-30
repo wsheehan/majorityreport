3aeeeb902b2293cc73cb0ce8d2d4586cc9e65aa6
Will Sheehan <willsheehan95@gmail.com>
2019-08-30 13:16:06 -0400

    add link in precedents to precedent page

--- src/components/Precedents/index.js
+++ src/components/Precedents/index.js
@@ -6,37 +6,6 @@ import * as API from '../../api'
 
 import './style.scss'
 
-const precedents = [
-  {
-    id: 1,
-    description: "The question is subjective in nature",
-    rationale: "Will not reliably lead to a winning fork",
-    invalids: 30,
-    notables: ["Bastille", "Will this Market Fork"]
-  },
-  {
-    id: 2,
-    description: "The outcome was not known at market end time",
-    rationale: "Reporters cannot know what happened as it has not occured, with some events this means that the initial reporter could report correctly the before the event happens, but that result ends up being incorrect",
-    invalids: 61,
-    notables: ["Senate"]
-  },
-  {
-    id: 3,
-    description: "The title, details, and outcomes are in direct conflict with each other",
-    rationale: "No schelling point on correct answer other than invalid",
-    invalids: 15,
-    notables: ["Veil Prices", "BTC $1000"]
-  },
-  {
-    id: 4,
-    description: "There are strong arguments for the market resolving as multiple outcomes",
-    rationale: "Reduces incentive to report if such markets are valid",
-    invalids: 23,
-    notables: ["ETH $500"]
-  }
-]
-
 class Precedents extends Component {
   constructor() {
     super()
@@ -72,10 +41,10 @@ class Precedents extends Component {
       <div className="precedents container-fluid">
         <div className="row">
           {sortedPrecedents.map(p => (
-            <div className="precedent-wrapper col-sm-6">
+            <div className="precedent-wrapper col-sm-6" key={p.id}>
               <div className="precedent">
                 <span className="short-name">{p.name}</span>
-                <h5>{p.description}</h5>
+                <h5><Link className="dope-link" to={`/precedent/${p.id}`}>{p.description}</Link></h5>
                 <p>{p.rationale}</p>
                 <span className="precedent-invalids">Markets Invalidated: {p.markets} <span className="percent-of-invalids">{((p.markets / totalInvalids) * 100).toFixed(2)}%</span></span>
               </div>
