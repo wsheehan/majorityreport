5089e74167334f7bbc53af53aef7ba47fc6d6ce6
Will Sheehan <willsheehan95@gmail.com>
2019-08-30 09:16:40 -0400

    API endpoint based on ENV

--- src/api.js
+++ src/api.js
@@ -1,12 +1,26 @@
 import axios from 'axios'
 
 // connection to backend API
-const BASE_URL = "http://localhost:4000"
+const { REACT_APP_ENV } = process.env
+const BASE_URL = REACT_APP_ENV === "prod" 
+  ? "https://majority-report-api.herokuapp.com" 
+  : "http://localhost:4000"
 
 export async function precedents () {
   try {
     const { data } = await axios.get(
       `${BASE_URL}/precedents`
+    )
+    return data
+  } catch (e) {
+    return { err: e }
+  }
+}
+
+export async function market (id) {
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}/market/${id}`
     )
     return data
   } catch (e) {
