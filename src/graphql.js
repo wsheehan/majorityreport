70d218b4201669a579a18ca18fc5054ec5b25b96
Will Sheehan <willsheehan95@gmail.com>
2019-08-30 13:16:20 -0400

    correct error handling in graphql.js

--- src/graphql.js
+++ src/graphql.js
@@ -4,12 +4,11 @@ const url = "https://api.thegraph.com/subgraphs/name/wsheehan/majority-report"
 
 async function graphql (query) {
   const postData = JSON.stringify({ query })
-  const res = await axios.post(url, postData)
-
-  if (res.status !== 200 || res.data.errors) {
-    res.data.errors.map(console.error)
-  } else {
-    return res.data.data
+  try {
+    const res = await axios.post(url, postData)
+    return res.data.data    
+  } catch (err) {
+    console.error(err)
   }
 }
 
